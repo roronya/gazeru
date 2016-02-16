d9b60394ab4254504ef1b4fdd7f3505ad30d1154
roronya <roronya628@gmail.com>
2016-02-17 06:39:33 +0900

    v1.1.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='gazeru',
-    version='1.1.1',
+    version='1.1.2',
     description='A crawler for niconico',
     long_description=long_description,
     url='http://github.com/roronya/gazeru',
